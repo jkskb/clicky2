6b94445ccbc6651c9c64f5709275d8f7ca825b43
Kate Ryan <jkskb@yahoo.com>
2019-01-27 22:47:26 -0600

    puppy clicky game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/card/card";
+import Wrapper from "./components/wrapper/wrapper";
+import Score from "./components/score/score";
+import pups from "./cards.json";
+import "./App.css";
 
 class App extends Component {
+  // Setting this.state.pups to the cards json array
+  state = {
+    pups,
+    clickedPuppyIds: [],
+    score: 0,
+    goal: 8,
+    status: ""
+  };
+
+  //shuffle the pup cards in the browser when clicked
+  shuffleScoreCard = id => {
+    let clickedPuppyIds = this.state.clickedPuppyIds;
+
+    if(clickedPuppyIds.includes(id)){
+      this.setState({ clickedPuppyIds: [], score: 0, status:  "Game Over! You lost. Click to play again!" });
+      return;
+    }else{
+      clickedPuppyIds.push(id)
+
+      if(clickedPuppyIds.length === 8){
+        this.setState({score: 8, status: "You sure are one smart puppy! Click to play again!", clickedPuppyIds: []});
+        console.log('Winner Winner Chicken Dinner');
+        return;
+      }
+
+      this.setState({ pups, clickedPuppyIds, score: clickedPuppyIds.length, status: " " });
+
+      for (let i = pups.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [pups[i], pups[j]] = [pups[j], pups[i]];
+      }
+    }
+  }
+
+  // Map over this.state.cards and render a Card component for each card object
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <h1 className="App-title">Clicky Game</h1>
+          <p className="App-intro">
+            Click All of the Puppies Just One Time to WIN!!!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
-      </div>
+        <Score total={this.state.score}
+               goal={8}
+               status={this.state.status}
+               />
+        <Wrapper>
+          {this.state.pups.map(puppy => (
+            <Card
+              shuffleScoreCard={this.shuffleScoreCard}
+              id={puppy.id}
+              key={puppy.id}
+              image={puppy.image}
+            />
+          ))}
+        </Wrapper>
+    </div>
     );
   }
 }
